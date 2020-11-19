7bf56639d71bb4a0b79bf8e11280b91f35c9d8ac
daniblast <dani.correia001@gmail.com>
2020-11-19 17:18:15 +0000

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,12 +1,12 @@
 #ifndef LCD_H_
 #define LCD_H_
 
-#define LCDPORT         GPIO_PORTB_BASE
-#define LCDPORTENABLE   SYSCTL_PERIPH_GPIOB
+#define LCDPORT         GPIO_PORTD_BASE
+#define LCDPORTENABLE   SYSCTL_PERIPH_GPIOD
 #define RS              GPIO_PIN_0
 #define E               GPIO_PIN_1
-#define D4              GPIO_PIN_4
-#define D5              GPIO_PIN_5
+#define D4              GPIO_PIN_2
+#define D5              GPIO_PIN_3
 #define D6              GPIO_PIN_6
 #define D7              GPIO_PIN_7
 
